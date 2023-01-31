022381aa4f4b5e65df82aaf767c3954ac263668f
Bahar Taskin <cakirbhr7@gmail.com>
2023-02-01 00:18:51 +0300

    update

---
+++ src/test/java/tests/WithDeleting.java
@@ -0,0 +1,51 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import testBase.TestBase;
+import utilities.Driver;
+import utilities.SleepMethod;
+
+public class WithDeleting extends TestBase {
+
+    @Test
+    public void test1(){
+
+
+        WebElement javaScriptTab = Driver.getDriver().findElement(By.xpath("//div[text()='JavaScript']"));
+        javaScriptTab.click();
+        SleepMethod.sleep(2);
+
+        WebElement polymerLink = Driver.getDriver().findElement(By.xpath("//a[@href='examples/polymer/index.html']"));
+        polymerLink.click();
+        SleepMethod.sleep(2);
+
+
+        WebElement newTodoInput = Driver.getDriver().findElement(By.xpath("//input[@id='new-todo']"));
+        newTodoInput.sendKeys("First Item",Keys.ENTER);
+        SleepMethod.sleep(2);
+
+        newTodoInput.sendKeys("Second Item", Keys.ENTER);
+        SleepMethod.sleep(2);
+
+        WebElement secondItemCheckBox = Driver.getDriver().findElement(By.xpath("(//input[@class='toggle style-scope td-item'])[2]"));
+        secondItemCheckBox.click();
+        SleepMethod.sleep(2);
+
+        WebElement clearCompletedButton = Driver.getDriver().findElement(By.xpath("//button[@class='clear-completed style-scope td-todos']"));
+        clearCompletedButton.click();
+        SleepMethod.sleep(2);
+
+        newTodoInput.sendKeys("Second Item Edit", Keys.ENTER);
+        SleepMethod.sleep(2);
+
+
+
+
+
+    }
+
+}
--- src/test/java/tests/WithDoubleClick.java
+++ src/test/java/tests/WithDoubleClick.java
@@ -3,17 +3,19 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-
 import testBase.TestBase;
 import utilities.Driver;
 import utilities.SleepMethod;
 
-public class BasicBrowserAutomationFramework extends TestBase {
+public class WithDoubleClick extends TestBase {
 
     @Test
     public void test1(){
 
+
+        //I use moveToElement method for third step
 
         WebElement javaScriptTab = Driver.getDriver().findElement(By.xpath("//div[text()='JavaScript']"));
         javaScriptTab.click();
@@ -25,22 +27,30 @@ public class BasicBrowserAutomationFramework extends TestBase {
 
 
         WebElement newTodoInput = Driver.getDriver().findElement(By.xpath("//input[@id='new-todo']"));
-        newTodoInput.sendKeys("First Item",Keys.ENTER);
+        newTodoInput.sendKeys("First Item", Keys.ENTER);
         SleepMethod.sleep(2);
 
         newTodoInput.sendKeys("Second Item", Keys.ENTER);
         SleepMethod.sleep(2);
 
-        WebElement secondItemCheckBox = Driver.getDriver().findElement(By.xpath("(//input[@class='toggle style-scope td-item'])[2]"));
-        secondItemCheckBox.click();
+        //moveToElement Method
+        Actions actions = new Actions(Driver.getDriver());
+
+        WebElement secondItem = Driver.getDriver().findElement(By.xpath("//label[text()='Second Item']"));
+        actions.moveToElement(secondItem).doubleClick().sendKeys(" Edit").perform();
         SleepMethod.sleep(2);
 
-        WebElement clearCompletedButton = Driver.getDriver().findElement(By.xpath("//button[@class='clear-completed style-scope td-todos']"));
-        clearCompletedButton.click();
-        SleepMethod.sleep(2);
 
-        newTodoInput.sendKeys("Second Item Edit", Keys.ENTER);
-        SleepMethod.sleep(2);
+
+        //WebElement secondItemEdit = Driver.getDriver().findElement(By.xpath("( //input[@id='edit'])[2]"));
+
+      /*  WebElement xButton = Driver.getDriver().findElement(By.xpath("(//button[@class='destroy style-scope td-item'])[2]"));
+        actions.moveToElement(xButton).click().perform();
+        SleepMethod.sleep(2);*/
+
+
+
+
 
 
 
